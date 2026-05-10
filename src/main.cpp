dcdb7f24f6c211fcf66dea937369ea6388e6ef3c
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-05-10 16:42:38 +0100

    Implement close on save feature in MyAccountLayer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,69 @@
 
 using namespace geode::prelude;
 
+#if defined(GEODE_IS_ANDROID) || defined(GEODE_IS_IOS)
+
+class $modify(MyAccountLayer, AccountLayer) {
+    struct Fields {
+        bool m_closeOnSave = false;
+        CCMenuItemToggler* m_closeToggle = nullptr;
+    };
+    void onCloseToggle(CCObject* sender) {
+        m_fields->m_closeOnSave = !m_fields->m_closeOnSave;
+    }
+    virtual void customSetup() {
+        AccountLayer::customSetup();
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        auto toggleOn  = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
+        auto toggleOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+        auto toggle = CCMenuItemToggler::create(toggleOff, toggleOn, this, menu_selector(MyAccountLayer::onCloseToggle));
+        toggle->setScale(0.7f);
+        toggle->toggle(false);
+        m_fields->m_closeToggle = toggle;
+        auto label = CCLabelBMFont::create("close GD after successful save", "bigFont.fnt");
+        label->setScale(0.35f);
+        label->setAnchorPoint({ 0.f, 0.5f });
+        auto menu = CCMenu::create();
+        menu->setPosition(ccp(winSize.width / 2 - 120.f, 38.f));
+        menu->setContentSize({ 250.f, 30.f });
+        toggle->setPosition(ccp(0.f, 0.f));
+        menu->addChild(toggle);
+        label->setPosition(ccp(14.f, 0.f));
+        menu->addChild(label);
+        this->addChild(menu, 10);
+    }
+    virtual void backupAccountFinished() {
+        AccountLayer::backupAccountFinished();
+        if (m_fields->m_closeOnSave) {
+            this->runAction(CCSequence::create(
+                CCDelayTime::create(0.8f),
+                CCCallFunc::create(this, callfunc_selector(MyAccountLayer::doQuitGame)),
+                nullptr
+            ));
+        }
+    }
+    virtual void syncAccountFinished() {
+        AccountLayer::syncAccountFinished();
+        if (m_fields->m_closeOnSave) {
+            this->runAction(CCSequence::create(
+                CCDelayTime::create(0.8f),
+                CCCallFunc::create(this, callfunc_selector(MyAccountLayer::doQuitGame)),
+                nullptr
+            ));
+        }
+    }
+    void doQuitGame() {
+        GameManager::sharedState()->save();
+        LocalLevelManager::sharedState()->save();
+        CCDirector::sharedDirector()->end();
+#if defined(GEODE_IS_IOS) || defined(GEODE_IS_MACOS)
+        exit(0); // my gf told me about it since shes an iphoner
+#endif
+    }
+};
+
+#else
+
 enum class PostSaveAction {
     NOTHING, CLOSE_GD, SHUTDOWN_PC, RESTART_PC, SLEEP_PC, CLOSE_AND_SLEEP
 };
@@ -183,3 +246,5 @@ class $modify(MyAccountLayer, AccountLayer) {
         }
     }
 };
+
+#endif
