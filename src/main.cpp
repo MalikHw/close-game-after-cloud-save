cb2b6ac3861918b8ea1a60816134fdd767f63988
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-05-11 11:20:01 +0100

    Refactor post-save actions and toggle settings

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,17 @@ class $modify(MyAccountLayer, AccountLayer) {
     };
     void onCloseToggle(CCObject* sender) {
         m_fields->m_closeOnSave = !m_fields->m_closeOnSave;
+        Mod::get()->setSavedValue("close-on-save", m_fields->m_closeOnSave);
     }
     virtual void customSetup() {
         AccountLayer::customSetup();
+        m_fields->m_closeOnSave = Mod::get()->getSavedValue<bool>("close-on-save", false);
         auto winSize = CCDirector::sharedDirector()->getWinSize();
         auto toggleOn  = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
         auto toggleOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
         auto toggle = CCMenuItemToggler::create(toggleOff, toggleOn, this, menu_selector(MyAccountLayer::onCloseToggle));
         toggle->setScale(0.7f);
-        toggle->toggle(false);
+        toggle->toggle(m_fields->m_closeOnSave);
         m_fields->m_closeToggle = toggle;
         auto label = CCLabelBMFont::create("close GD after successful save", "bigFont.fnt");
         label->setScale(0.35f);
@@ -70,110 +72,70 @@ enum class PostSaveAction {
     NOTHING, CLOSE_GD, SHUTDOWN_PC, RESTART_PC, SLEEP_PC, CLOSE_AND_SLEEP
 };
 
-class PostSavePopup : public geode::Popup {
-protected:
-    std::function<void(PostSaveAction)> m_callback;
-    PostSaveAction m_selectedAction = PostSaveAction::NOTHING;
-    std::vector<CCMenuItemToggler*> m_radioToggles;
-    
-    bool init(std::function<void(PostSaveAction)> callback) {
-        if (!Popup::init(280.f, 240.f))
-            return false;
-        
-        m_callback = callback;
-        this->setTitle("After Save Action");
-        auto winSize = CCSize{280.f, 240.f};
-        std::vector<std::pair<std::string, PostSaveAction>> options = {
-            {"Do Nothing", PostSaveAction::NOTHING},
-            {"Close GD", PostSaveAction::CLOSE_GD},
-            {"Shutdown PC", PostSaveAction::SHUTDOWN_PC},
-            {"Restart PC", PostSaveAction::RESTART_PC},
-            {"Sleep PC", PostSaveAction::SLEEP_PC},
-            {"Close GD + Sleep PC", PostSaveAction::CLOSE_AND_SLEEP}
-        };
-        float startY = winSize.height / 2 + 30.f + 44.f;
-        for (size_t i = 0; i < options.size(); i++) {
-            auto& [text, action] = options[i];
-            auto toggleOn = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
-            auto toggleOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
-            auto toggle = CCMenuItemToggler::create(toggleOff, toggleOn, this, menu_selector(PostSavePopup::onRadioToggle));
-            toggle->setScale(0.6f);
-            toggle->setTag(static_cast<int>(action));
-            toggle->toggle(action == PostSaveAction::NOTHING);
-            auto label = CCLabelBMFont::create(text.c_str(), "bigFont.fnt");
-            label->setScale(0.45f);
-            label->setAnchorPoint({0.f, 0.5f});
-            auto menu = CCMenu::create();
-            menu->setPosition(ccp(winSize.width / 2 - 80.f, startY - i * 28.f));
-            menu->setContentSize({200.f, 25.f});
-            toggle->setPosition(ccp(0.f, 0.f));
-            menu->addChild(toggle);
-            label->setPosition(ccp(20.f, 0.f));
-            menu->addChild(label);
-            m_mainLayer->addChild(menu);
-            m_radioToggles.push_back(toggle);
-        }
-        auto okSprite = ButtonSprite::create("OK", "goldFont.fnt", "GJ_button_01.png", 0.8f);
-        auto okBtn = CCMenuItemSpriteExtra::create(okSprite, this, menu_selector(PostSavePopup::onOK));
-        auto btnMenu = CCMenu::create();
-        btnMenu->addChild(okBtn);
-        btnMenu->setPosition(winSize.width / 2, 25.f);
-        m_mainLayer->addChild(btnMenu);
-        return true;
-    }
-    void onRadioToggle(CCObject* sender) {
-        auto clicked = static_cast<CCMenuItemToggler*>(sender);
-        m_selectedAction = static_cast<PostSaveAction>(clicked->getTag());
-        for (auto toggle : m_radioToggles) {
-            bool shouldBeOn = (static_cast<PostSaveAction>(toggle->getTag()) == m_selectedAction);
-            if (toggle->isToggled() != shouldBeOn) {
-                toggle->toggle(shouldBeOn);
-            }
-        }
-    }
-    void onOK(CCObject*) {
-        if (m_callback) m_callback(m_selectedAction);
-        this->onClose(nullptr);
-    }
-public:
-    static PostSavePopup* create(std::function<void(PostSaveAction)> callback) {
-        auto ret = new PostSavePopup();
-        if (ret->init(callback)) {
-            ret->autorelease();
-            return ret;
-        }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-};
-
 class $modify(MyAccountLayer, AccountLayer) {
     struct Fields {
         PostSaveAction m_postSaveAction = PostSaveAction::NOTHING;
         CCLabelBMFont* m_statusLabel = nullptr;
         bool m_hasShownWarning = false;
+        bool m_isWine = false;
     };
-    void onActionButton(CCObject*) {
-        PostSavePopup::create([this](PostSaveAction action) {
-            m_fields->m_postSaveAction = action;
-            updateStatusLabel();
-        })->show();
+    
+    void checkWineAndLoadAction() {
+#ifdef GEODE_IS_WINDOWS
+        if (std::getenv("WINEPREFIX")) {
+            m_fields->m_isWine = true;
+            m_fields->m_postSaveAction = Mod::get()->getSavedValue<bool>("close-on-save", false) ? PostSaveAction::CLOSE_GD : PostSaveAction::NOTHING;
+        } else {
+            m_fields->m_isWine = false;
+            auto actionStr = Mod::get()->getSavedValue<std::string>("post-save-action", "nothing");
+            if (actionStr == "close") m_fields->m_postSaveAction = PostSaveAction::CLOSE_GD;
+            else if (actionStr == "shutdown") m_fields->m_postSaveAction = PostSaveAction::SHUTDOWN_PC;
+            else if (actionStr == "restart") m_fields->m_postSaveAction = PostSaveAction::RESTART_PC;
+            else if (actionStr == "sleep") m_fields->m_postSaveAction = PostSaveAction::SLEEP_PC;
+            else if (actionStr == "close-and-sleep") m_fields->m_postSaveAction = PostSaveAction::CLOSE_AND_SLEEP;
+            else m_fields->m_postSaveAction = PostSaveAction::NOTHING;
+        }
+#elif defined(GEODE_IS_MACOS)
+        m_fields->m_isWine = false;
+        auto actionStr = Mod::get()->getSavedValue<std::string>("post-save-action", "nothing");
+        if (actionStr == "close") m_fields->m_postSaveAction = PostSaveAction::CLOSE_GD;
+        else if (actionStr == "shutdown") m_fields->m_postSaveAction = PostSaveAction::SHUTDOWN_PC;
+        else if (actionStr == "restart") m_fields->m_postSaveAction = PostSaveAction::RESTART_PC;
+        else if (actionStr == "sleep") m_fields->m_postSaveAction = PostSaveAction::SLEEP_PC;
+        else if (actionStr == "close-and-sleep") m_fields->m_postSaveAction = PostSaveAction::CLOSE_AND_SLEEP;
+        else m_fields->m_postSaveAction = PostSaveAction::NOTHING;
+#endif
     }
+    
+    void onCloseToggle(CCObject* sender) {
+        bool newValue = !Mod::get()->getSavedValue<bool>("close-on-save", false);
+        Mod::get()->setSavedValue("close-on-save", newValue);
+        m_fields->m_postSaveAction = newValue ? PostSaveAction::CLOSE_GD : PostSaveAction::NOTHING;
+        updateStatusLabel();
+    }
+    
     void updateStatusLabel() {
         if (!m_fields->m_statusLabel) return;
         std::string text;
-        switch (m_fields->m_postSaveAction) {
-            case PostSaveAction::NOTHING: text = "Action: None"; break;
-            case PostSaveAction::CLOSE_GD: text = "Action: Close GD"; break;
-            case PostSaveAction::SHUTDOWN_PC: text = "Action: Shutdown PC"; break;
-            case PostSaveAction::RESTART_PC: text = "Action: Restart PC"; break;
-            case PostSaveAction::SLEEP_PC: text = "Action: Sleep PC"; break;
-            case PostSaveAction::CLOSE_AND_SLEEP: text = "Action: Close + Sleep"; break;
+        if (m_fields->m_isWine) {
+            text = Mod::get()->getSavedValue<bool>("close-on-save", false) ? "Will close GD" : "Won't close GD";
+        } else {
+            switch (m_fields->m_postSaveAction) {
+                case PostSaveAction::NOTHING: text = "Action: None"; break;
+                case PostSaveAction::CLOSE_GD: text = "Action: Close GD"; break;
+                case PostSaveAction::SHUTDOWN_PC: text = "Action: Shutdown PC"; break;
+                case PostSaveAction::RESTART_PC: text = "Action: Restart PC"; break;
+                case PostSaveAction::SLEEP_PC: text = "Action: Sleep PC"; break;
+                case PostSaveAction::CLOSE_AND_SLEEP: text = "Action: Close + Sleep"; break;
+            }
         }
         m_fields->m_statusLabel->setString(text.c_str());
     }
+    
     virtual void customSetup() {
         AccountLayer::customSetup();
+        
+        checkWineAndLoadAction();
         
         if (!m_fields->m_hasShownWarning && Loader::get()->isModLoaded("beefyandtheducks.save-on-exit")) {
             m_fields->m_hasShownWarning = true;
@@ -181,21 +143,40 @@ class $modify(MyAccountLayer, AccountLayer) {
         }
         
         auto winSize = CCDirector::sharedDirector()->getWinSize();
-        auto buttonSprite = ButtonSprite::create("After Save...", "goldFont.fnt", "GJ_button_04.png", 0.8f);
-        buttonSprite->setScale(0.7f);
-        auto button = CCMenuItemSpriteExtra::create(buttonSprite, this, menu_selector(MyAccountLayer::onActionButton));
-        auto statusLabel = CCLabelBMFont::create("Action: None", "bigFont.fnt");
-        statusLabel->setScale(0.3f);
-        m_fields->m_statusLabel = statusLabel;
-        auto menu = CCMenu::create();
-        menu->setPosition(ccp(283.5f, 217.f));
-        menu->setContentSize({250.f, 50.f});
-        button->setPosition(ccp(0.f, 10.f));
-        menu->addChild(button);
-        statusLabel->setPosition(ccp(0.f, -10.f));
-        menu->addChild(statusLabel);
-        this->addChild(menu, 10);
+        
+        if (m_fields->m_isWine) {
+            // Wine: show simple toggle like mobile
+            auto toggleOn  = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
+            auto toggleOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+            auto toggle = CCMenuItemToggler::create(toggleOff, toggleOn, this, menu_selector(MyAccountLayer::onCloseToggle));
+            toggle->setScale(0.7f);
+            toggle->toggle(Mod::get()->getSavedValue<bool>("close-on-save", false));
+            auto label = CCLabelBMFont::create("close GD after successful save", "bigFont.fnt");
+            label->setScale(0.35f);
+            label->setAnchorPoint({ 0.f, 0.5f });
+            auto menu = CCMenu::create();
+            menu->setPosition(ccp(180.5f, 227.f));
+            menu->setContentSize({ 250.f, 30.f });
+            toggle->setPosition(ccp(0.f, 0.f));
+            menu->addChild(toggle);
+            label->setPosition(ccp(14.f, 0.f));
+            menu->addChild(label);
+            this->addChild(menu, 10);
+        } else {
+            // Native Windows/macOS: show status label
+            auto statusLabel = CCLabelBMFont::create("", "bigFont.fnt");
+            statusLabel->setScale(0.3f);
+            m_fields->m_statusLabel = statusLabel;
+            auto menu = CCMenu::create();
+            menu->setPosition(ccp(283.5f, 217.f));
+            menu->setContentSize({250.f, 50.f});
+            statusLabel->setPosition(ccp(0.f, 0.f));
+            menu->addChild(statusLabel);
+            this->addChild(menu, 10);
+            updateStatusLabel();
+        }
     }
+    
     virtual void backupAccountFinished() {
         AccountLayer::backupAccountFinished();
         if (m_fields->m_postSaveAction == PostSaveAction::NOTHING) return;
@@ -218,42 +199,26 @@ class $modify(MyAccountLayer, AccountLayer) {
         auto action = m_fields->m_postSaveAction;
         if (action == PostSaveAction::SHUTDOWN_PC) {
 #ifdef GEODE_IS_WINDOWS
-            if (std::getenv("WINEPREFIX")) {
-                system("systemctl poweroff");
-            } else {
-                system("shutdown /s /t 4");
-            }
+            system("shutdown /s /t 4");
 #elif defined(GEODE_IS_MACOS)
             system("osascript -e 'tell app \"System Events\" to shut down'");
 #endif
         } else if (action == PostSaveAction::RESTART_PC) {
 #ifdef GEODE_IS_WINDOWS
-            if (std::getenv("WINEPREFIX")) {
-                system("systemctl reboot");
-            } else {
-                system("shutdown /r /t 4");
-            }
+            system("shutdown /r /t 4");
 #elif defined(GEODE_IS_MACOS)
             system("osascript -e 'tell app \"System Events\" to restart'");
 #endif
         } else if (action == PostSaveAction::SLEEP_PC) {
 #ifdef GEODE_IS_WINDOWS
-            if (std::getenv("WINEPREFIX")) {
-                system("systemctl suspend");
-            } else {
-                system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
-            }
+            system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
 #elif defined(GEODE_IS_MACOS)
             system("pmset sleepnow");
 #endif
             return; // dont close gd
         } else if (action == PostSaveAction::CLOSE_AND_SLEEP) {
 #ifdef GEODE_IS_WINDOWS
-            if (std::getenv("WINEPREFIX")) {
-                system("systemctl suspend &");
-            } else {
-                system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
-            }
+            system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
 #elif defined(GEODE_IS_MACOS)
             system("pmset sleepnow &");
 #endif
